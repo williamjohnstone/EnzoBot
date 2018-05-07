be8f95a7fcb06f6887a52bd16d1b82ea94e5af17
William Johnstone <37273113+GravityGamer@users.noreply.github.com>
2018-05-07 17:29:30 +0100

    Fixed error and removed deprecated Methods

--- src/main/java/gravity/gbot/utils/Logging/msgLogger.java
+++ src/main/java/gravity/gbot/utils/Logging/msgLogger.java
@@ -6,25 +6,31 @@ import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 public class msgLogger {
-    public void log(GuildMessageReceivedEvent event, String prefix) {
+    public void log(GuildMessageReceivedEvent event, String BotPrefix) {
         String args[] = event.getMessage().getContentRaw().split(" +");
         if (Config.loggingALL) {
             if (!event.getAuthor().isBot()) {
                 if (event.getChannel().getType() == ChannelType.PRIVATE) {
-                    System.out.println("[GravityBot] Message Received, Channel: " + event.getMessage().getChannel().getName() + ", Channel Type: " + event.getChannelType() + ", Author: " + event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator() + ", Message: " + event.getMessage().getContentRaw());
+                    System.out.println("[GravityBot] Message Received, Channel: " + event.getMessage().getChannel().getName() + ", Channel Type: " + event.getChannel().getType() + ", Author: " + event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator() + ", Message: " + event.getMessage().getContentRaw());
                 } else if (event.getChannel().getType() == ChannelType.TEXT) {
-                    System.out.println("[GravityBot] Message Received, Channel: " + event.getMessage().getChannel().getName() + ", Channel Type: " + event.getChannelType() + ", Author: " + event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator() + ", Message: " + event.getMessage().getContentRaw() + ", Guild (Server): " + event.getGuild().getName());
+                    System.out.println("[GravityBot] Message Received, Channel: " + event.getMessage().getChannel().getName() + ", Channel Type: " + event.getChannel().getType() + ", Author: " + event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator() + ", Message: " + event.getMessage().getContentRaw() + ", Guild (Server): " + event.getGuild().getName());
                 }
             }
         }
+        String msg = event.getMessage().getContentRaw().toLowerCase();
+        if(msg.startsWith(BotPrefix)) {
+            msg = msg.substring(BotPrefix.length());
+        }
+        String[] parts = msg.split(" +");
+        String commandName = parts[0];
         BotListener listner = new BotListener();
-        if (listner.getCommand(args[0], prefix, event) != null) {
+        if (listner.getCommand(commandName) != null) {
             if (Config.loggingCMD && !Config.loggingALL) {
                 if (!event.getAuthor().isBot()) {
                     if (event.getChannel().getType() == ChannelType.PRIVATE) {
-                        System.out.println("[GravityBot] Message Received, Channel: " + event.getMessage().getChannel().getName() + ", Channel Type: " + event.getChannelType() + ", Author: " + event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator() + ", Message: " + event.getMessage().getContentRaw());
+                        System.out.println("[GravityBot] Message Received, Channel: " + event.getMessage().getChannel().getName() + ", Channel Type: " + event.getChannel().getType() + ", Author: " + event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator() + ", Message: " + event.getMessage().getContentRaw());
                     } else if (event.getChannel().getType() == ChannelType.TEXT) {
-                        System.out.println("[GravityBot] Message Received, Channel: " + event.getMessage().getChannel().getName() + ", Channel Type: " + event.getChannelType() + ", Author: " + event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator() + ", Message: " + event.getMessage().getContentRaw() + ", Guild (Server): " + event.getGuild().getName());
+                        System.out.println("[GravityBot] Message Received, Channel: " + event.getMessage().getChannel().getName() + ", Channel Type: " + event.getChannel().getType() + ", Author: " + event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator() + ", Message: " + event.getMessage().getContentRaw() + ", Guild (Server): " + event.getGuild().getName());
                     }
                 }
             }
